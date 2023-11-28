c4bbb9d42aadf4f2d48576eef0df646d144cd06d
darshnagdeve35 <darshnagdeve35@gmail.com>
2023-11-27 22:42:58 -0500

    First commit and init the proj

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  return (
+    <>
+      <form action="" method="post">
+        <label>bruh</label>
+        <input
+          type="text"
+          id="Email"
+          value={Email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <label>text</label>
+        <input
+          type="text"
+          name="text"
+          id=""
+          value={Password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
